d40bee83c572d367508898e53a8ab05ea940e893
ahmetpalavan <ahmetpalavann@gmail.com>
2023-09-22 20:50:39 +0300

    redirection activated

--- components/ActionRow.tsx
+++ components/ActionRow.tsx
@@ -1,6 +1,11 @@
 import { Ionicons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import { Text, TouchableOpacity } from "react-native";
+import { RootStackParamList } from "../App";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+
+export type NavigationProp = NativeStackNavigationProp<RootStackParamList, "Home">;
 
 interface ActionRowProps {
   title: string;
@@ -12,14 +17,21 @@ interface ActionRowProps {
 }
 
 const ActionRow = (props: ActionRowProps) => {
+  const navigation = useNavigation<NavigationProp>();
   return (
     <TouchableOpacity
+      onPress={() => {
+        navigation.navigate(props.screen);
+      }}
+      className={`flex m-2 flex-1 py-6 justify-center items-center rounded-lg shadow-lg space-x-2
+        ${props.vertical ? "flex-col" : "flex-row"}
+      `}
       style={{
         backgroundColor: props.color,
       }}
     >
       <Ionicons name={props.icon} size={30} color={"white"} />
-      <Text>{props.title}</Text>
+      <Text className="text-white font-bold italic">{props.title}</Text>
     </TouchableOpacity>
   );
 };
